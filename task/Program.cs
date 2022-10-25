9564a2d8149193d1c6920d5adff5b0e7f8532d72
Arina <0730205@mail.ru>
2022-10-25 22:43:33 +0300

    Добавили решение задачи

---
+++ task/Program.cs
@@ -0,0 +1,42 @@
+﻿int size = 5;
+string[] array = new string[size];
+
+Console.WriteLine("Введите с клавиатуры любое количество символов 5 раз через enter (допустимы как буквы, так и цифры): ");
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine("Строковый массив с элементами меньше или равно 3-х: ");
+Sorted(array);
+
+void FillArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Console.ReadLine()!;
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine(" " + array[i]);
+    }
+  
+}
+void Sorted(string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            count++;
+            Console.Write(array[i] + " ");
+        }
+    }
+    if (count == 0)
+        {
+           Console.Write("В данном массиве нет элементов меньше или равном 3-м!"); 
+        }
+}
